6796300a2c0ce96e61b25f024cc9a9432ae450fb
Yulia <yulia.dmitrieva@yahoo.com>
2022-05-28 11:51:14 +0300

    first commit

---
+++ src/test/java/guru_qa/FirstTest.java
@@ -0,0 +1,13 @@
+package guru_qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+
+}
